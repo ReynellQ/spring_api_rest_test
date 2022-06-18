03f6da5ea2febaa239249df3754a07a1d0fee7eb
Reynell Quevedo <reynell200501@gmail.com>
2022-06-17 20:50:09 -0500

    Init project

---
+++ src/main/java/com/project/market_test/MarketTestApplication.java
@@ -0,0 +1,14 @@
+package com.project.market_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MarketTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/market_test/MarketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.market_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
